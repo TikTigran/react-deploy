58a0418a0e3eae41d763f7703e8e8f4837bafa89
Tigran Budumyan <tigranbudumyan@gmail.com>
2021-08-12 18:42:38 +0400

    Changed coming soon component

--- src/App/App.js
+++ src/App/App.js
@@ -1,19 +1,14 @@
-import React, { Suspense, lazy } from "react";
-import { Route, Switch } from "react-router";
+import React from "react";
+import ComingSoon from "src/pages/coming-soon/ComingSoon";
 // STYLES
 import "./app.scss";
 // PAGES
-const ComingSoonPage = lazy(() => import("src/pages/coming-soon/ComingSoon"));
 
 const App = () => {
   return (
     <div className="app">
       <div className="wrapper">
-        <Suspense fallback={<h1>Loading...</h1>}>
-          <Switch>
-            <Route path="/" exact component={ComingSoonPage} />
-          </Switch>
-        </Suspense>
+        <ComingSoon />
       </div>
     </div>
   );
